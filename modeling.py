1848504796b068bba063c68026267ddf29bda47b
Luo Ruikun <adamluo1995@gmail.com>
2019-07-22 10:06:24 +0800

    nothing

--- modeling.py
+++ modeling.py
@@ -6,20 +6,25 @@ import random
 
 
 class BertForCoQA(BertPreTrainedModel):
-    def __init__(self,
-                 config,
-                 output_attentions=False,
-                 keep_multihead_output=False,
-                 cls_alpha=1.0,
-                 mask_p=0.):
+    def __init__(
+            self,
+            config,
+            output_attentions=False,
+            keep_multihead_output=False,
+            cls_alpha=1.0,
+            mask_p=0.0,
+    ):
         super(BertForCoQA, self).__init__(config)
         self.cls_alpha = cls_alpha
         self.mask_p = mask_p
         self.output_attentions = output_attentions
-        self.bert = BertModel(config,
-                              output_attentions=output_attentions,
-                              keep_multihead_output=keep_multihead_output)
+        self.bert = BertModel(
+            config,
+            output_attentions=output_attentions,
+            keep_multihead_output=keep_multihead_output,
+        )
         # self.qa_outputs_mid = nn.Linear(config.hidden_size, config.hidden_size)
+        # self.dropout = nn.Dropout(config.hidden_dropout_prob)  # NOTE: It hurts.
         self.qa_outputs = nn.Linear(config.hidden_size, 2)
         # self.cls_outputs_mid = nn.Linear(config.hidden_size,
         #                                  config.hidden_size)
@@ -37,26 +42,29 @@ class BertForCoQA(BertPreTrainedModel):
             head_mask=None,
     ):
         # mask some words on inputs_ids
-        if self.training and self.mask_p > 0:
-            batch_size = input_ids.size(0)
-            for i in range(batch_size):
-                len_c, len_qc = token_type_ids[i].sum(
-                    dim=0).detach().item(), attention_mask[i].sum(
-                        dim=0).detach().item()
-                masked_idx = random.sample(range(len_qc - len_c, len_qc),
-                                           int(len_c * self.mask_p))
-                input_ids[i, masked_idx] = 100
+        # if self.training and self.mask_p > 0:
+        #     batch_size = input_ids.size(0)
+        #     for i in range(batch_size):
+        #         len_c, len_qc = token_type_ids[i].sum(
+        #             dim=0).detach().item(), attention_mask[i].sum(
+        #                 dim=0).detach().item()
+        #         masked_idx = random.sample(range(len_qc - len_c, len_qc),
+        #                                    int(len_c * self.mask_p))
+        #         input_ids[i, masked_idx] = 100
 
-        outputs = self.bert(input_ids,
-                            token_type_ids,
-                            attention_mask,
-                            output_all_encoded_layers=False,
-                            head_mask=head_mask)
+        outputs = self.bert(
+            input_ids,
+            token_type_ids,
+            attention_mask,
+            output_all_encoded_layers=False,
+            head_mask=head_mask,
+        )
         if self.output_attentions:
             all_attentions, sequence_output, cls_outputs = outputs
         else:
             sequence_output, cls_outputs = outputs
         span_logits = self.qa_outputs(sequence_output)
+        # cls_outputs = self.dropout(cls_outputs)
         cls_logits = self.cls_outputs(cls_outputs)
 
         start_logits, end_logits = span_logits.split(1, dim=-1)
